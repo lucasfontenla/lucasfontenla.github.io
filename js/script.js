20ebe5d18c305d6c17d4c7d05d32c5b633be9f01
lucasfontenla <lucascfontenla@hotmail.com>
2016-12-20 07:38:43 -0200

    07:38

--- js/script.js
+++ js/script.js
@@ -15,15 +15,25 @@
 		});
 	}
 
+	//SMALL SCREEN ADAPTOR
 	if (screen.width < 600){
 		$("footer h5").hide();
 		$("footer a i").removeClass("right");
 		$(".container").addClass("center-align");
+		$("#navbar_pin").removeClass("navbar-fixed");
 	}
 	else {
 		$("footer h5").show();
 		$(".container").removeClass("center-align");
 		$("footer a i").addClass("right");
+		$("#navbar_pin").addClass("navbar-fixed");
 	}
+
+	$('.button-collapse').sideNav({
+		menuWidth: 200,
+		edge: 'left',
+		draggable: true
+	});
+
   });
 })(jQuery);

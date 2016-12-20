7f0a73c606e1705e6b5843f6115907ec5f463585
lucasfontenla <lucascfontenla@hotmail.com>
2016-12-20 06:56:10 -0200

    06:56

--- js/script.js
+++ js/script.js
@@ -14,5 +14,16 @@
 				$("#profile_picture").attr("src", "imgs/profile_picture.jpg");
 		});
 	}
+
+	if (screen.width < 600){
+		$("footer h5").hide();
+		$("footer a i").removeClass("right");
+		$(".container").addClass("center-align");
+	}
+	else {
+		$("footer h5").show();
+		$(".container").removeClass("center-align");
+		$("footer a i").addClass("right");
+	}
   });
 })(jQuery);

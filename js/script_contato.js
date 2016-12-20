70056a1aceec8db5ae17bad4b3369f9065bd8200
lucasfontenla <lucascfontenla@hotmail.com>
2016-12-20 10:40:26 -0200

    10:40

--- js/script_contato.js
+++ js/script_contato.js
@@ -4,5 +4,13 @@
   	//SIDE NAV
 	$('.button-collapse').sideNav();
 
+	//SMALL SCREEN ADAPTOR
+	if (screen.width < 600){
+		$("#navbar_pin").removeClass("navbar-fixed");
+	}
+	else {
+		$("#navbar_pin").addClass("navbar-fixed");
+	}
+
   });
 })(jQuery);

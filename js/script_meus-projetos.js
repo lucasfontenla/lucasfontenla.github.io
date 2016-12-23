1fdf501f97fa2b97e9dd2b45bd8fa3bc1f81a9e1
Lucas Fontenla <lucascfontenla@hotmail.com>
2016-12-23 14:46:58 -0200

    23/12/16 14h46

---
+++ js/script_meus-projetos.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+  	//SIDE NAV
+	$('.button-collapse').sideNav();
+
+	//SMALL SCREEN ADAPTOR
+	if (screen.width < 600){
+		$("header #navbar_pin").removeClass("navbar-fixed");
+	}
+	else {
+		$("header #navbar_pin").addClass("navbar-fixed");
+	}
+	
+  });
+})(jQuery);

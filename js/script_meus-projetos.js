eeb2c47f1d736f3d25b62e26f418e54724418389
Lucas Fontenla <lucascfontenla@hotmail.com>
2016-12-24 00:40:37 -0200

    00:40

--- js/script_meus-projetos.js
+++ js/script_meus-projetos.js
@@ -12,5 +12,19 @@
 		$("header #navbar_pin").addClass("navbar-fixed");
 	}
 	
+	//SMALL SCREEN ADAPTOR
+	if (screen.width < 600){
+		$("footer h5").hide();
+		$("footer a i").removeClass("right");
+		$(".container").addClass("center-align");
+		$("#navbar_pin").removeClass("navbar-fixed");
+	}
+	else {
+		$("footer h5").show();
+		$(".container").removeClass("center-align");
+		$("footer a i").addClass("right");
+		$("#navbar_pin").addClass("navbar-fixed");
+	}
+	
   });
 })(jQuery);

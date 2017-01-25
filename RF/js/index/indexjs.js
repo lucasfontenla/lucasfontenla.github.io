58d78811077e1774cd010ddd10466c324be278cf
Lucas Fontenla <lucascfontenla@hotmail.com>
2017-01-25 15:19:28 -0200

    RF Atualizado

--- RF/js/index/indexjs.js
+++ RF/js/index/indexjs.js
@@ -9,23 +9,41 @@
     var NavHeight = $('nav').height();
     $('div.parallax-container').css({"margin-top":("-"+String(NavHeight)+"px")})
 
+    if(screen.width < 600){
+    	var scrollLimit = 400;
+    }
+    else {
+    	var scrollLimit = 500;
+    }
+
     $(window).on('scroll', function(){
 		var scrollPos = $(window).scrollTop();
 
-		if(scrollPos > 550){
+		if(scrollPos > scrollLimit){
 			$('div.first').fadeOut(150, function(){
 				$('div.second').fadeIn(150);
 				$("nav").css({"background-image":"linear-gradient(#263238, #263238)"});
 			});
 		}
 
-		else if(scrollPos <= 550){
+		else if(scrollPos <= scrollLimit){
 			$('div.second').fadeOut(150, function(){
 				$('div.first').fadeIn(150);
-				$("nav").css({"background-image":"linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,.5), rgba(0,0,0,.2))"});
+				if(screen.width > 600){
+					$("nav").css({"background-image":"linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,.5), rgba(0,0,0,.2))"});
+				}
+				else {
+					$("nav").css({"background-image":"linear-gradient(to right, rgba(0,0,0,.3), rgba(0,0,0,.75), rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,0))"});
+				}		
 			});
 		}
 	});
+
+	var screenHeight = window.innerHeight;
+	var imageHeight = $("div.parallax-container.index").height();
+	var firstSectionHeight = screenHeight - imageHeight;
+
+	$("div.section.first").css({"height":(String(firstSectionHeight)+"px")});
 
   });
 })(jQuery);
